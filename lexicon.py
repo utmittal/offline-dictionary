3fdcbda6388adc2e0ed9b617de1c0149a8b81cae
Utkarsh Mittal <mittal.utk@gmail.com>
2024-10-07 18:51:21 +1100

    Add WordMeaning object.
    This will serve as our internal representation of a word, regardless of the dictionary that is used. I expect this to expand with time.

--- lexicon.py
+++ lexicon.py
@@ -1,10 +1,20 @@
 import json
 import string
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
 
 from util.project_path import project_path
+
+
+@dataclass(frozen=True)
+class WordMeaning:
+    word: str
+    definition: str
+    part_of_speech: str | None
+    sentence: str | None
+    synonyms: list[str] | None
 
 
 class LexiconSource(Enum):
@@ -13,13 +23,15 @@ class LexiconSource(Enum):
 
 class Lexicon(ABC):
     @abstractmethod
-    def define(self, word: str) -> list[str]:
+    def define(self, word: str) -> list[WordMeaning]:
         pass
 
 
 class LexiconWordset(Lexicon):
     @staticmethod
     def __get_file_path(first_letter: str) -> Path:
+        first_letter = first_letter.lower()
+
         if len(first_letter) > 1:
             raise ValueError(
                 f"Argument 'first_letter' must be single character. Got {first_letter} with length = "
@@ -30,10 +42,11 @@ class LexiconWordset(Lexicon):
         else:
             file_name = "misc.json"
 
+        # TODO: use project_root / <path> representation
         return project_path(f'datasets/wordset/data/{file_name}')
 
     @staticmethod
-    def __get_dict_for(first_letter: str) -> dict[str, dict[str, str | list]]:
+    def __get_dict_for(first_letter: str) -> dict[str, dict[str, str | list[dict[str, str]]]]:
         if len(first_letter) > 1:
             raise ValueError(
                 f"Argument 'first_letter' must be single character. Got {first_letter} with length = "
@@ -45,15 +58,28 @@ class LexiconWordset(Lexicon):
 
         return word_dict
 
-    def define(self, word: str) -> list[str] | None:
-        first_letter = word[0].lower()
-        letter_dict = LexiconWordset.__get_dict_for(first_letter)
+    @staticmethod
+    def __wordset_dict_to_word_meaning_obj(wordset_dict: dict[str, str | list[dict[str, str]]]) -> list[WordMeaning]:
+        meanings_list = []
+
+        word = wordset_dict['word']
+        for meaning in wordset_dict['meanings']:
+            definition = meaning['def']
+            part_of_speech = meaning['speech_part'] if 'speech_part' in meaning else None
+            synonyms = meaning['synonyms'] if 'synonyms' in meaning else None
+            sentence = meaning['example'] if 'example' in meaning else None
+
+            meanings_list.append(WordMeaning(word, definition, part_of_speech, sentence, synonyms))
+
+        return meanings_list
+
+    def define(self, word: str) -> list[WordMeaning] | None:
+        letter_dict = LexiconWordset.__get_dict_for(word[0])
 
         if word not in letter_dict:
             return None
         else:
-            word_data = letter_dict[word]
-            return [meaning['def'] for meaning in word_data['meanings']]
+            return LexiconWordset.__wordset_dict_to_word_meaning_obj(letter_dict[word])
 
 
 def lexicon_from(dict_source: LexiconSource) -> Lexicon:
